280e7dff04c6684b8be98def2a4761614c1d0118
AgusBenitez99 <agustin.newells99@gmail.com>
2023-06-25 21:26:31 -0300

    armado de estructura

---
+++ app.js
@@ -0,0 +1,12 @@
+const { log } = require('console');
+const express=require('express');
+const path=require('path');
+
+const app=express();
+const PORT=3030;
+//para decirle a express (middewares)
+app.use(express.static('public'))
+
+/*rutas*/
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
+app.listen(PORT,()=>console.log(`Server running in http://localhost:${PORT}`));
